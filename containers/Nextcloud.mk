5cd6b81adfbc34ad7fb99be1472a7427777547a1
c-4422 <c-4422@protonmail.com>
2022-01-02 16:41:03 -0600

    Add password names to makefile
    Make it easier to find out what pass entries are used in
    containers by listing the names.

--- containers/Nextcloud.mk
+++ containers/Nextcloud.mk
@@ -6,6 +6,11 @@ DATABASE=next_db
 NEXTCLOUD_PORT=8000
 DATABASE_PORT=3306
 
+# Pass names
+NEXTCLOUD_PASS=nextcloud_admin
+NEXTCLOUD_DB=next_db
+NEXTCLOUD_DB_ROOT=next_db_root
+
 container:
 	@echo "Making nextcloud"
 	@echo "IP_ADDRESS = $(IP_ADDRESS)"
@@ -15,6 +20,10 @@ name:
 
 port:
 	@echo "$(NEXTCLOUD_PORT)/$(DATABASE_PORT)"
+
+password:
+	@printf '%s:\t%s\n' "$(NEXTCLOUD)" "$(NEXTCLOUD_PASS)" | expand -t 15
+	@printf '%s:\t%s, %s\n' "$(DATABASE)" "$(NEXTCLOUD_DB_ROOT)" "$(NEXTCLOUD_DB)" | expand -t 15
 
 start:
 	@echo "Starting $(NEXTCLOUD) container"
